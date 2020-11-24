240f1a2b3409cf7e57035dbaf3e0495fb929a1d7
LotuxPunk <vandendaelenclement@gmail.com>
2020-11-24 16:28:36 +0100

    ✨ Added login route

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,7 @@
 const UserORM = require("../orm/model/User")
-const {getHash} = require("../utils/utils");
+const {getHash, compareHash} = require("../utils/utils");
+const jwt = require('jsonwebtoken');
+const process = require('process');
 
 module.exports.createUser = async (req, res) => {
     const body = req.body;
@@ -28,4 +30,44 @@ module.exports.createUser = async (req, res) => {
             res.sendStatus(500);
         }
     }
-}
+};
+
+module.exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    if(email === undefined && password == undefined){
+        res.sendStatus(400);
+    }
+    else {
+        try {
+            const user = await UserORM.findOne({where: {email:email}})
+            if(user !== null && await compareHash(password, user.password)){
+                const {id, firstname, lastname} = user;
+                if (user.role <= 5) {
+                    const payload = {status: "owner", value: {id, firstname, lastname}};
+                    const token = jwt.sign(
+                        payload,
+                        process.env.SECRET_TOKEN,
+                        {expiresIn: '1d'}
+                    );
+                    res.json(token);
+                }
+                else{
+                    const payload = {status: "tenant", value: {id, firstname, lastname}};
+                    const token = jwt.sign(
+                        payload,
+                        process.env.SECRET_TOKEN,
+                        {expiresIn: '1d'}
+                    );
+                    res.json(token);
+                }                
+            }
+            else{
+                res.sendStatus(404);
+            }
+        }
+        catch(err){
+            console.error(err);
+            res.sendStatus(500);
+        }
+    }
+};
--- route/user.js
+++ route/user.js
@@ -4,5 +4,6 @@ const Router = require("express-promise-router");
 const router = new Router;
 
 router.post("/", UserController.createUser);
+router.post("/login", UserController.loginUser)
 
 module.exports = router;
