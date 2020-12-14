547c1ad6d4e2921d98a52b6a2200b08f21347455
LotuxPunk <vandendaelenclement@gmail.com>
2020-12-14 15:58:32 +0100

    🐛 Avoid non image type file

--- route/apartment.js
+++ route/apartment.js
@@ -11,15 +11,22 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/')
     },
-    filename : (req, file, cb) => {
+    filename: (req, file, cb) => {
         let id = shortid.generate();
         let ext = mime.extension(file.mimetype);
         cb(null, `${id}.${ext}`);
     }
 })
-const upload = multer({ 
-    mimetype : /^image/,
-    storage:storage
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
 });
 
 router.get("/", identification, mustBeOwner, ApartmentController.getApartments);
