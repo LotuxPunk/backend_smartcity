7caa8b4018af054755682d67cbc0683c739b745b
LotuxPunk <vandendaelenclement@gmail.com>
2020-12-14 15:58:02 +0100

    ✨ Added image route

--- route/index.js
+++ route/index.js
@@ -4,11 +4,13 @@ const ContractRouter = require("./contract");
 const PaymentRouter = require("./payment");
 const TenantRouter = require("./tenant");
 const router = require("express").Router();
+const express = require('express');
 
 router.use("/user", UserRouter);
 router.use("/apartment", ApartmentRouter);
 router.use("/contract", ContractRouter);
 router.use("/payment", PaymentRouter);
 router.use("/tenant", TenantRouter);
+router.use("/media", express.static('./uploads'));
 
 module.exports = router;
